0ab601cc15d7fc5e0151631755af432a1e25ae99
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-06-22 20:12:30 +0200

    Create JokezApplication

---
+++ src/main/java/io/github/wkktoria/jokez/JokezApplication.java
@@ -0,0 +1,16 @@
+package io.github.wkktoria.jokez;
+
+import io.github.wkktoria.jokez.api.JokeApiResponse;
+import io.github.wkktoria.jokez.controller.JokeController;
+import io.github.wkktoria.jokez.service.JokeService;
+
+class JokezApplication {
+    public static void main(String[] args) {
+        JokeService jokeService = new JokeService();
+        JokeController jokeController = new JokeController(jokeService);
+
+        JokeApiResponse apiResponse = jokeController.randomJoke();
+        String joke = apiResponse.getSetup() + "\n" + apiResponse.getPunchline();
+        System.out.println(joke);
+    }
+}
